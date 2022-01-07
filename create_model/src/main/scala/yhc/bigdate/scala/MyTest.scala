e502bc1a98b2cf7bdabed5629372fdf6da2ed33c
yanghaocheng <yanghaocheng@wsecar.com>
2022-01-07 09:48:48 +0800

    flink mysql cdc test

---
+++ create_model/src/main/scala/yhc/bigdate/scala/MyTest.scala
@@ -0,0 +1,19 @@
+package yhc.bigdate.scala
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @author ：_my
+ * @date ：Created in 2022/1/4 15:52
+ * @description：${description }
+ * @version: 1.0
+ */
+object MyTest {
+  def main(args: Array[String]): Unit = {
+
+    val sc = new SparkContext
+
+
+  }
+
+}
